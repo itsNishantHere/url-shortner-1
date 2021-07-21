6fe34528cafdb9de353cbceb67aa532e4f8efb60
Nishant <nishantsharmansb@gmail.com>
2021-07-21 18:33:43 +0530

    Add env vars

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/urlshortner", {
+mongoose.connect(process.env.MONGODB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
